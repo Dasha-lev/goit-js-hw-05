4babfafe306808ad08980ca4e740d50f3f709a5e
Daria <darina.levchuk@gmail.com>
2025-02-09 18:45:22 +0200

    fix task-3 & task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,6 @@
 const getTotalBalanceByGender = (users, gender) => {
     return users
+        .filter(user => user.gender === gender); 
         .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 }
 
